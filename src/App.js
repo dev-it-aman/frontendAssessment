bd30ee773d647bf5ac7a1dad522fb243ca909191
amansingh1906 <singhamankumar98@gmail.com>
2023-09-13 00:57:40 +0530

    firsrt commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JobCard from './JobCard';
+
+function App() {
+  return (
+    <div className="App">
+       <JobCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/JobCard.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+const JobCard = () => {
+    return (
+        <div className="Frame281171 w-[1873px] h-[399px] pl-[85px] pr-[44.88px] pt-[30px] pb-[49px] bg-zinc-300 justify-end items-start gap-[83.12px] inline-flex">
+        <div className="JobCard w-[830px] self-stretch px-6 py-4 bg-white rounded-[10px] border border-gray-300 flex-col justify-start items-start gap-2.5 inline-flex">
+          <div className="Frame280562 self-stretch justify-between items-start inline-flex">
+            <div className="Frame280556 w-[357px] self-stretch justify-start items-start gap-2 flex">
+              <img className="Rectangle1965 w-12 h-12 rounded-[5px]" src="https://via.placeholder.com/48x48" />
+              <div className="Frame9765 flex-col justify-start items-start gap-5 inline-flex">
+                <div className="Frame9764 flex-col justify-start items-start gap-6 flex">
+                  <div className="Frame9763 flex-col justify-start items-start flex">
+                    <div className="UxUiDesigner text-black text-2xl font-normal font-['Poppins'] leading-loose">UX UI Designer</div>
+                    <div className="GreatVibesInformationTechnology text-black text-base font-normal font-['Poppins'] leading-normal">Great Vibes - Information Technology</div>
+                    <div className="ChennaiTamilnaduIndiaInOffice text-neutral-500 text-base font-normal font-['Poppins'] leading-normal">Chennai, Tamilnadu, India (In-office)</div>
+                  </div>
+                  <div className="Frame9762 self-stretch h-[120px] flex-col justify-start items-start gap-2 flex">
+                    <div className="Frame9297 flex-col justify-start items-start gap-2.5 flex">
+                      <div className="PartTime900Am500PmIst text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">Part-Time (9.00 am - 5.00 pm IST)</div>
+                    </div>
+                    <div className="Frame9298 justify-start items-start gap-2.5 inline-flex">
+                      <div className="Experience12Years text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">Experience (1 - 2 years)</div>
+                    </div>
+                    <div className="Frame280554 justify-start items-start gap-2.5 inline-flex">
+                      <div className="Inr3000060000Month text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">INR (₹) 30,000 - 60,000 / Month</div>
+                    </div>
+                    <div className="Frame280553 justify-start items-start gap-2.5 inline-flex">
+                      <div className="200Employees text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">51-200 employees</div>
+                    </div>
+                  </div>
+                  <div className="Frame280588 flex-col justify-center items-start gap-1 flex">
+                    <div className="Frame280608 flex-col justify-center items-start gap-[5px] flex">
+                      <div className="Frame280629 justify-start items-center gap-6 inline-flex">
+                        <div className="Buttons justify-start items-start flex">
+                          <div className="ButtonLg px-4 py-2 bg-sky-500 rounded-md shadow justify-center items-center flex">
+                            <div className="Text text-white text-base font-medium font-['Poppins'] leading-normal">Apply Now</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="Frame280561" />
+          </div>
+        </div>
+        <div className="JobCard w-[830px] self-stretch px-6 py-4 bg-white rounded-[10px] border border-gray-300 flex-col justify-start items-start gap-2.5 inline-flex">
+          <div className="Frame280562 self-stretch justify-between items-start inline-flex">
+            <div className="Frame280556 w-[357px] self-stretch justify-start items-start gap-2 flex">
+              <img className="Rectangle1965 w-12 h-12 rounded-[5px]" src="https://via.placeholder.com/48x48" />
+              <div className="Frame9765 flex-col justify-start items-start gap-5 inline-flex">
+                <div className="Frame9764 flex-col justify-start items-start gap-6 flex">
+                  <div className="Frame9763 flex-col justify-start items-start flex">
+                    <div className="Developer text-black text-2xl font-normal font-['Poppins'] leading-loose">Developer</div>
+                    <div className="GreatVibesInformationTechnology text-black text-base font-normal font-['Poppins'] leading-normal">Great Vibes - Information Technology</div>
+                    <div className="ChennaiTamilnaduIndiaInOffice text-neutral-500 text-base font-normal font-['Poppins'] leading-normal">Chennai, Tamilnadu, India (In-office)</div>
+                  </div>
+                  <div className="Frame9762 self-stretch h-[120px] flex-col justify-start items-start gap-2 flex">
+                    <div className="Frame9297 flex-col justify-start items-start gap-2.5 flex">
+                      <div className="PartTime900Am500PmIst text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">Part-Time (9.00 am - 5.00 pm IST)</div>
+                    </div>
+                    <div className="Frame9298 justify-start items-start gap-2.5 inline-flex">
+                      <div className="Experience12Years text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">Experience (1 - 2 years)</div>
+                    </div>
+                    <div className="Frame280554 justify-start items-start gap-2.5 inline-flex">
+                      <div className="Inr5000070000Month text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">INR (₹) 50,000 - 70,000 / Month</div>
+                    </div>
+                    <div className="Frame280553 justify-start items-start gap-2.5 inline-flex">
+                      <div className="200Employees text-neutral-800 text-base font-normal font-['Poppins'] leading-normal">51-200 employees</div>
+                    </div>
+                  </div>
+                  <div className="Frame280588 flex-col justify-center items-start gap-1 flex">
+                    <div className="Frame280608 flex-col justify-center items-start gap-[5px] flex">
+                      <div className="Frame280629 justify-start items-center gap-6 inline-flex">
+                        <div className="ButtonSecondary px-4 py-2 bg-white rounded-[5px] border border-sky-500 justify-start items-center gap-2 flex">
+                          <div className="Apply text-center text-sky-500 text-base font-medium font-['Poppins'] leading-normal">External Apply</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="Frame280561" />
+          </div>
+        </div>
+      </div>
+    )
+};
+
+export default JobCard;
